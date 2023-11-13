4877429151030fb40a1f28e7aa9505ea08a69b8a
StefanIstrati <145895345+StefanIstrati@users.noreply.github.com>
2023-11-13 10:46:00 +0200

    Update Lexer.c

--- Lexer.c
+++ Lexer.c
@@ -40,32 +40,86 @@ void append_int(Node **root, char *name) {
         curr->next = new_node;
     }
 }
+void append_float(Node **root, char *name) {
+    Node *new_node = malloc(sizeof(Node));
+    Node *curr;
+    if (new_node == NULL) {
+        exit(1);
+    }
+    new_node->next = NULL;
+    new_node->var.FLOAT = 0;
+    new_node->type = FLOAT;
+    new_node->var_name = strdup(name); // Allocate memory for the name
+    if (*root == NULL) {
+        *root = new_node;
+    } else {
+        curr = *root;
+        while (curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+    }
+}
 
 void Var(Node **root, char **token) {
     int i;
     if(strcmp(token[1], "int") == 0){
         for(i=2;token[i] != NULL; i++){
             append_int(root,token[i]);
+        }
+    }else if(strcmp(token[1], "float") == 0){
+        for(i=2;token[i] != NULL; i++){
+            append_float(root,token[i]);
+        }
+    }else{
+        printf("Undefined type of variables ");
     }
+}
+ 
+void Input(Node **root, char **token) {
+    int i;
+    Node *curr;
+    curr = *root;
+    while (curr != NULL) {
+        if (strcmp(curr->var_name, token[1]) == 0) {
+            if (curr->type == INTEGER) {
+                curr->var.INT = atoi(token[3]);
+                break;
+            } else if (curr->type == FLOAT) {  
+                curr->var.FLOAT = atof(token[3]);
+                break;
+            } else {
+                printf("Undefined variable %s", token[1]);
+                break;
+            }
+        }
+        curr = curr->next;
+    }
+    if (curr == NULL) {
+        printf("Undefined variable %s", token[1]);
     }
 }
 
-/*void Input(Node **root,char **token){
-    int i=0;
-    if (root != NULL) {
-            for (curr = root; curr != NULL; curr = curr->next) {
-                if (curr->var_name == token[1]) {
-                    i++;
-
-                }
-                if (i == 0){
-                    printf("Variable not declared:%s",token[1]);
-                }
-            }
-        }
-
-
-}*/
+void Calc(Node **root, char **token){
+    int i;
+    Node *curr;
+    curr = *root;
+    float final;
+    curr = *root;
+    for (i=0;token[i] != NULL;i++){
+        printf("%s",token[i]);
+    }
+    /*switch (expression)
+    {
+    case /* constant-expression */
+        /* code 
+        break;
+    
+    default:
+        break;
+    }
+    */
+}
 
 int ver(char *index) {
     if (strcmp(index, "var") == 0) {
@@ -89,8 +143,8 @@ void Deallocate(Node *root) {
     }
 }
 
-int token(char *line, char ***tokens) {
-    char *token = strtok(line, " \t\n;=");
+void token(char *line, char ***tokens) {
+    char *token = strtok(line, " \t\n;:");
     int tokenCount = 0;
 
     *tokens = (char **)malloc(sizeof(char *) * 100);
@@ -110,7 +164,6 @@ int token(char *line, char ***tokens) {
 
     // Null-terminate the array of tokens
     (*tokens)[tokenCount] = NULL;
-    return tokenCount;
 }
 
 
@@ -125,15 +178,21 @@ int main() {
     char c,d;
     char index[20] = "";
     Node *curr;
-    while(getc(file) != EOF){
+    while(1){
         i=0;
         line = (char *)malloc(sizeof(char));
-        while ((c = getc(file)) != ';' &&  c != EOF) {
+        while ((c = getc(file)) != ';') {
+            if(c == EOF){
+            break;
+            }else{
             line[i] = c;
             i++;
             line = (char *)realloc(line, (i + 1) * sizeof(char));
+            }
         }
-
+        if(c == EOF){
+            break;
+        }
         line[i] = '\0';
         token(line,&tokens);
         free(line);
@@ -142,7 +201,9 @@ int main() {
         if (p == 1) {
             Var(&root,tokens);
         }else if(p == 2){
-           // Input(&root,tokens);
+            Input(&root,tokens);
+        }else if (p == 3){
+            Calc(&root,tokens);
         }
 
         for(i=0;tokens[i] != NULL;i++){
@@ -150,10 +211,15 @@ int main() {
         }free(tokens);
         getc(file);
     }
+    end:
     curr=root;
-               for (curr = root; curr != NULL; curr = curr->next) {
-                    printf("\n%s:%d ", curr->var_name, curr->var.INT);
-               }
+                for (curr = root; curr != NULL; curr = curr->next) {
+                    if (curr->type == INTEGER){
+                        printf("\n%s:%d INTEGER", curr->var_name, curr->var.INT);
+                    }else{
+                        printf("\n%s:%f FLOAT", curr->var_name, curr->var.FLOAT);
+                    }
+                }
                 // Add similar handling for FLOAT case if needed
     Deallocate(root);
     return 0;
